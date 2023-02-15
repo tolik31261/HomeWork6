4c79b63f2bbbef1b088a9119bf40220e1093bcd7
User <nazarovtolya2@gmail.com>
2023-02-15 17:40:07 +0500

    Задачи 1-4

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,37 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
+        task3();
+        task4();
     }
 
     public static void task1 () {
         System.out.println("Задача 1");
+        for (int i = 1; i<=10; i++) {
+            System.out.println("Интерация " + i);
+        }
+    }
 
+    public static void task2() {
+        System.out.println("Задача 2");
+        for (int i=10; i>=1; i--) {
+            System.out.println("Интерация " + i);
+        }
+    }
+
+    public static void task3() {
+        System.out.println("Задача 3");
+        for (int i=0; i<=17; i=i+2)
+            if (i % 2 == 0) {
+            System.out.println("Интерация " + i);
+        }
+    }
+
+    public static void task4() {
+        System.out.println("Задача 4");
+        for (int i=10; i>=-10; i--) {
+            System.out.println("Интерация " + i);
+        }
     }
 }
